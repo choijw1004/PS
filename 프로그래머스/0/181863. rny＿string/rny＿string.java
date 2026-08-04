3cba03147d6b4f6bb39f64d3bd8f7af2c0ac9fcf
최장우 <97choijw@gmail.com>
2026-08-04 13:35:44 +0900

    [level 0] Title: rny_string, Time: 0.06 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String rny_string) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(var c : rny_string.toCharArray()){
+            if(c == 'm') sb.append("rn");
+            
+            else sb.append(c);
+        }
+        
+        return sb.toString();
+    }
+}
