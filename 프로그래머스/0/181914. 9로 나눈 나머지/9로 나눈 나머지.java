6c9af9ef1ae7063d39c8a41b244b543e3e7faca2
최장우 <97choijw@gmail.com>
2026-08-06 10:04:35 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 2.55 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String number) {
+        int sum = 0;
+        
+        for(var c : number.toCharArray()){
+            sum += c - '0';
+        }
+        
+        return sum % 9;
+    }
+}
