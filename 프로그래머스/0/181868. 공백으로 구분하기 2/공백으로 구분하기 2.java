188cb1277e84b2bf8e15fe6129b627b40a143c91
최장우 <97choijw@gmail.com>
2026-08-06 13:26:35 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 1.43 ms, Memory: 83.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String[] solution(String my_string) {
+        int n = my_string.length();
+        String[] arr = my_string.trim().split("\\s+");
+    
+        return arr;
+    }
+}
