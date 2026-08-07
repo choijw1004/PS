d481b734d6daad4ab0d6ef2a5e0e4c2710c694b2
최장우 <97choijw@gmail.com>
2026-08-07 11:27:13 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 21.80 ms, Memory: 87 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,9 @@
+class Solution {
+    public long solution(long n) {
+        for(long i = 1; i * i <= n; i++){
+            if(n / i == i && n % i == 0) return (i + 1) * (i + 1);
+        }
+        
+        return -1;
+    }
+}
